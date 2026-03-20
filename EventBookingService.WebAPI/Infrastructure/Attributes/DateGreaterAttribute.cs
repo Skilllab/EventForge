5a7391d2f434ccd018f475431c84fdfa76d54ef0
Oleg Bezyaev <redslide@yandex.ru>
2026-03-20 14:37:07 +0300

    Добавил тесты валидации модели EventsFilter

--- EventBookingService.WebAPI/Infrastructure/Attributes/DateGreaterAttribute.cs
+++ EventBookingService.WebAPI/Infrastructure/Attributes/DateGreaterAttribute.cs
@@ -19,11 +19,17 @@ namespace EventBookingService.WebAPI.Infrastructure.Attributes
         {
             var curDate = (DateTime?)value;
             var property = validationContext.ObjectType.GetProperty(_comparisonProperty);
-            var comparisonDate = (DateTime?)property?.GetValue(validationContext.ObjectInstance);
 
-            if (curDate < comparisonDate)
+            if (property == null)
+                throw new ArgumentException("Свойство не найдено");
+
+            var comparisonDate = (DateTime?)property.GetValue(validationContext.ObjectInstance);
+
+            if (curDate.HasValue && comparisonDate.HasValue && curDate < comparisonDate)
             {
-                return new ValidationResult(ErrorMessage ?? "Дата окончания события не может быть раньше даты начала");
+                return new ValidationResult(ErrorMessage ?? "Дата окончания события не может быть раньше даты начала",
+                    new[] { validationContext.MemberName! }
+                );
             }
 
             return ValidationResult.Success;
