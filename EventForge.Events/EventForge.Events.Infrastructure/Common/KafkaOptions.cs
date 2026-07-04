cd818c9e8834e95f0e437d5762c7e4104a84886e
Oleg Bezyaev <redslide@yandex.ru>
2026-07-04 10:35:29 +0300

    Добавил kafka в контейнеры и в проекты

---
+++ EventForge.Events/EventForge.Events.Infrastructure/Common/KafkaOptions.cs
@@ -0,0 +1,12 @@
+namespace EventForge.Events.Infrastructure.Common;
+
+/// <summary>
+/// Настройки подключения к Kafka
+/// </summary>
+public class KafkaOptions
+{
+    /// <summary>
+    /// Адрес Kafka bootstrap servers
+    /// </summary>
+    public string BootstrapServers { get; set; } = string.Empty;
+}
